b0ae5950bf97747a571767e80ff4a43fc55e638a
abhisekmisra <abhisekmisra@yahoo.com>
2021-06-29 11:23:59 +0530

    Add files via upload

---
+++ basic docker commands.ps1
@@ -0,0 +1,26 @@
+﻿# Get Docker version
+docker version
+
+# Get Docker images
+docker image ls
+
+# Pull hello-world image
+docker pull hello-world:latest
+
+# Run hello-world image
+docker container run hello-world
+
+# List currently running docker containers
+docker container ls
+
+# List all containers
+docker container ls -a
+
+# Remove docker container
+docker container rm 4faa
+
+# Remove hello-world image
+docker image rm  hello-world:latest
+
+# Check if image is removed
+docker image ls
